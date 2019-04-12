1f9097f579a3422ed40ecc004b56ba3d2eb0a9fe
oliviaclyde <olivialclyde@gmail.com>
2019-04-11 18:39:56 -0600

    1st commit

---
+++ elevators.py
@@ -0,0 +1,22 @@
+#Programming Challenge
+#Elevators in a building
+
+#Number of elevators 2
+#Number of floors 3
+
+#I would first create each elevator as an object
+#elevator1
+#properties
+#floor =
+#door = open or closed
+#status = occupied or not occupied
+#total_trips =
+#floors_traveled =
+
+#elevator2
+#properties
+#floor =
+#door = open or closed
+#status = occupied or not occupied
+#total_trips =
+#floors_traveled =
