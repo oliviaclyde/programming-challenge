f5cc28d284f2f76056f73b5d772b29c3ad514983
oliviaclyde <olivialclyde@gmail.com>
2019-04-11 19:35:31 -0600

    4th commit

--- elevators.py
+++ elevators.py
@@ -11,7 +11,7 @@ floors >= 1 and <= number_of_floors
 #properties
 #floor =
 #door = open or closed
-#status = occupied or not occupied
+#status = occupied or not occupied (this would represent elevator moving or not moving)
 #total_trips =
 #floors_traveled =
 
@@ -30,11 +30,22 @@ floors >= 1 and <= number_of_floors
 #It can't change any property of the elevators but can interact with them
 #controller
 def call ()
+#or
+#create a variable to represent the floor the call is made from call_floor
+call_floor = input("Select the floor: ")
 
-#call button is pushed on floor x
+#call button is pushed on call_floor
 #each elevator would need to relay what floor it is currently on
 #each elevator would need to relay what status it is in
-#elevator closest to responds
-if status = "unoccupied":
-    #will move to floor x
-    elif status =
+#when  call is made, each elevator would respond with its location and status
+#utilize if statments to prioritize which elevator responds
+#if elevator status == unoccupied and it is on the floor closest to call_floor, then elevator floor = call_floor
+#need to create a variable to determine which elevator is closer to the call_floor
+#if call_floor == 1 elevator_floor,
+#if elevator1 is on floor 2 and elevator2 is on floor 3, and the call_floor is 1, theh the logic would be
+if status == "unoccupied" and #reference logic :
+    elevator_floor = call_floor #this represents the elevator moving to the call_floor
+    elif floor == call_floor:
+        #the unoccupied elevator responds unless the other elevator is at the call floor
+else:
+    #won't move to call floor
