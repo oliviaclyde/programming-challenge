2bfee6dae540368e18b62276e8ecc3d21fbb7bfd
oliviaclyde <olivialclyde@gmail.com>
2019-04-11 19:00:42 -0600

    2nd commit

--- elevators.py
+++ elevators.py
@@ -20,3 +20,15 @@
 #status = occupied or not occupied
 #total_trips =
 #floors_traveled =
+
+#Would also need to provide for scaling the number of Elevators
+
+
+#I would need to create an object called controller to initiate a call button to summon the elevator
+#controller
+def call ()
+
+#call button is pushed on floor 1
+#each elevator would need to relay what floor it is currently on
+#each elevator would need to relay what status it is in
+#elevator closest to
