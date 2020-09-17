63e78101bddfad845325660aba1b51a531877ee3
oliviaclyde <olivialclyde@gmail.com>
2020-09-17 10:20:54 -0600

    considering how the elevator will respond to a call

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from threading import Thread
 
 def startController(controller):
     controller.start()
-
+    
 
 
 def main():
@@ -20,17 +20,22 @@ def main():
     
     controllerThread.start()
 
+
     endProgram = None
-    print("At any time create a call by entering origin and destination. (Ex. 1,3)")
+
+
+    print("At any time create a call by entering origin and destination. (Ex. 1,3) To quit, type Y")
     while endProgram is not "y" or "Y":
         selection = input()
         splitSelection = selection.split(',')
         origin = int(splitSelection[0])
         destination = int(splitSelection[1]) 
         controller.newCall(origin, destination)
-    
+        #controller.elevatorResponding(destination)
 
-
+   
+#  Need a way to exit
+# Need to debug if a character , or = is hit in error. Throws and error in the program
    
 if __name__ == "__main__":
     main()
